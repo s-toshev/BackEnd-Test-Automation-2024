17b0e122a7195407017bc2b92b2725e5d8b58a72
s-toshev <simeon.toshev@yahoo.bg>
2024-03-06 19:47:02 +0200

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+namespace ApiTestingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WeatherForecast weatherForecast = new WeatherForecast() 
+            {
+            Date = DateTime.Now,
+            TemperatureC = 32,
+            Summary = "New test summary"
+            };
+
+            string weatherForecastJson = JsonSerializer.Serialize(weatherForecast);
+
+            Console.WriteLine(weatherForecastJson);
+
+
+        }
+    }
+}
---
+++ Demo/WeatherForecast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTestingDemo
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public string Summary { get; set; }
+
+
+    }
+}
